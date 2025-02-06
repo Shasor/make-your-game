8d57638106acd7d57a9d9d05ef41fba22a59c911
Elmount <votre.email@example.com>
2025-02-06 17:05:08 +0100

    create enemy + health

---
+++ create/enemy_create.js
@@ -0,0 +1,18 @@
+import { Entity } from '../core/entities/entity.js';
+import { Position } from '../core/components/position_component.js';
+import { Velocity } from '../core/components/velocity_component.js';
+import { Visual } from '../core/components/visual_component.js';
+import { Health } from '../core/components/health_component.js';
+import { Property } from '../core/components/property_component.js';
+
+export function createEnemy(x = 0, y = 0, width = 40, height = 40, maxHealth = 100, color = 'blue') {
+    const enemy = new Entity();
+
+    // Composants de base
+    enemy.addComponent('position', new Position(x, y));
+    enemy.addComponent('visual', new Visual(color, height, width));
+    enemy.addComponent('health', new Health(maxHealth));
+    enemy.addComponent('property', new Property(false, 0, true)); // L'ennemi est immobile
+
+    return enemy;
+}
