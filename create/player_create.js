a7819024cceec397333177bddc1187823917f5a1
Adam GONCALVES <goncalvesadam@icloud.com>
2025-02-06 15:41:54 +0100

    createPlayer: add health component

--- create/player_create.js
+++ create/player_create.js
@@ -4,6 +4,7 @@ import { Velocity } from '../core/components/velocity_component.js';
 import { Visual } from '../core/components/visual_component.js';
 import { Input } from '../core/components/input_component.js';
 import { Feature } from '../core/components/feature_component.js';
+import { Health } from '../core/components/health_component.js';
 
 export function createPlayer(x = 0, y = 0, width = 32, height = 32, movable = false, speed = 5, color = 'purple') {
   const entity = new Entity();
@@ -12,6 +13,6 @@ export function createPlayer(x = 0, y = 0, width = 32, height = 32, movable = fa
   entity.addComponent('visual', new Visual(color, height, width));
   entity.addComponent('input', new Input());
   entity.addComponent('feature', new Feature(movable, speed));
-
+  entity.addComponent('health', new Health());
   return entity;
 }
